2d9e0768b6b7386c41629c2c4822c4b05748800e
bluedevil97 <xd2015_xd@hotmail.com>
2023-05-26 21:10:51 -0500

    diseño interfaces alumno, curso, docente, personal

---
+++ app/src/main/java/com/example/controladministrativo/Views/Cursos/CursoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.controladministrativo.Views.Cursos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.controladministrativo.R
+
+class CursoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_curso)
+    }
+}
---
+++ app/src/main/java/com/example/controladministrativo/Views/Personal/ListPersonalActivity.kt
@@ -0,0 +1,12 @@
+package com.example.controladministrativo.Views.Personal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.controladministrativo.R
+
+class ListPersonalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_personal)
+    }
+}
